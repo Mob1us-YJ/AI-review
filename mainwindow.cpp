1346ed679ab61b1ece6b4b2738ed3df8497d569a
mobius <mobius@mobiusdeMacBook-Air.local>
2025-08-06 14:24:40 +0800

    UI update by YJ

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -50,27 +50,45 @@ MainWindow::~MainWindow()
 
 void MainWindow::addSampleTasks()
 {
+    qDebug() << "=== 开始添加示例任务 ===";
+    
     // 保持原有的示例任务
     Form *form = new Form("1","高一（13）","数学","A3双面","待扫描","50","2025/8/2 8:07:25","扫描");
+    qDebug() << "创建扫描任务Form:" << form;
     ui->verticalLayout->addWidget(form);
+    qDebug() << "添加到verticalLayout";
     
     Form *form_1 = new Form("1","高一（12）","数学","A3双面","待扫描","50","2025/8/2 8:07:25","打印");
+    qDebug() << "创建打印任务Form_1:" << form_1;
     ui->verticalLayout_2->addWidget(form_1);
+    qDebug() << "添加到verticalLayout_2";
     
     Form *form_2 = new Form("2","高一（12）","语文","A3双面","待扫描","50","2025/8/2 8:07:25","打印");
+    qDebug() << "创建打印任务Form_2:" << form_2;
     ui->verticalLayout_4->addWidget(form_2);
+    qDebug() << "添加到verticalLayout_4";
     
     Form *form_3 = new Form("3","高一（12）","英语","A3双面","待扫描","50","2025/8/2 8:07:25","打印");
+    qDebug() << "创建打印任务Form_3:" << form_3;
     ui->verticalLayout_5->addWidget(form_3);
+    qDebug() << "添加到verticalLayout_5";
     
     Form *form_4 = new Form("4","高一（12）","物理","A3双面","待扫描","50","2025/8/2 8:07:25","打印");
+    qDebug() << "创建打印任务Form_4:" << form_4;
     ui->verticalLayout_6->addWidget(form_4);
+    qDebug() << "添加到verticalLayout_6";
     
     Form *form_5 = new Form("5","高一（12）","数学","A3双面","待扫描","50","2025/8/2 8:07:25","打印");
+    qDebug() << "创建打印任务Form_5:" << form_5;
     ui->verticalLayout_7->addWidget(form_5);
+    qDebug() << "添加到verticalLayout_7";
     
     Form *form_6 = new Form("6","高一（12）","数学","A3双面","待扫描","50","2025/8/2 8:07:25","打印");
+    qDebug() << "创建打印任务Form_6:" << form_6;
     ui->verticalLayout_8->addWidget(form_6);
+    qDebug() << "添加到verticalLayout_8";
+    
+    qDebug() << "=== 所有Form组件已添加到布局 ===";
     
     // 连接Form按钮信号
     connect(form, &Form::scanButtonClicked, this, &MainWindow::onFormScanButtonClicked);
@@ -80,6 +98,8 @@ void MainWindow::addSampleTasks()
     connect(form_4, &Form::printButtonClicked, this, &MainWindow::onFormPrintButtonClicked);
     connect(form_5, &Form::printButtonClicked, this, &MainWindow::onFormPrintButtonClicked);
     connect(form_6, &Form::printButtonClicked, this, &MainWindow::onFormPrintButtonClicked);
+    
+    qDebug() << "=== 信号连接完成 ===";
     
     // 启用模拟模式
     qDebug() << "\n=== 启用模拟模式 ===";
